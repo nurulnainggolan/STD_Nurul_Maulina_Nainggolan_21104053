5e4f38f595b6b861a734683ad39a44a4fdde8202
unknown <nurulnainggolan87@gmail.com>
2024-09-28 15:41:40 +0700

    01_Pengenalan_CPP_Bagian1

---
+++ TP/TP_Percabangan_if-else.cpp
@@ -0,0 +1,19 @@
+#ifndef TP_PERCABANGAN_IF-ELSE_CPP
+#define TP_PERCABANGAN_IF-ELSE_CPP
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int nilai;
+    cin >> nilai;
+    if (nilai > 80) {
+        cout << "A" << endl;
+    } else {
+        cout << "Bukan A" << endl;
+    }
+
+    return 0;
+}
+#endif // TP_PERCABANGAN_IF-ELSE_CPP
